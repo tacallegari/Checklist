1567ee604c62d72bd3384c09d3af75b1ace7f311
tacallegari <62042181+tacallegari@users.noreply.github.com>
2020-08-26 13:41:14 -0500

    Initial Commit

---
+++ Checklist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Checklist
+//
+//  Created by Tahlia Callegari on 8/26/20.
+//  Copyright © 2020 Tahlia Callegari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
